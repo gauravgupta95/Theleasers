16f3037f053444c964597c19a3ea9022dc9d192a
gauravgupta95 <gauravgupta_95@ymail.com>
2025-04-12 16:05:26 +0530

    Delete about.js

--- about.js
+++
@@ -1,9 +0,0 @@
-
-export default function About() {
-  return (
-    <main className="bg-black text-white min-h-screen p-10">
-      <h1 className="text-4xl text-yellow-400 mb-4">About Us</h1>
-      <p className="text-gray-300">We are Jaipur’s premium real estate agency specializing in commercial and luxury rentals.</p>
-    </main>
-  )
-}
